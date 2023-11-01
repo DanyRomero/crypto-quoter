41c50e1e64d35fd3f38745a43e3dd527192731bf
Dany Romero P <119898105+DanyRomeroP@users.noreply.github.com>
2023-11-01 13:00:51 -0600

    Print data of the convertion

--- src/components/Resultado.jsx
+++ src/components/Resultado.jsx
@@ -1,27 +1,59 @@
 /* eslint-disable react/prop-types */
-// import React from "react";
-// import styled from "@emotion/styled";
+
+import styled from "@emotion/styled";
+
+const Data = styled.div`
+  color: #fff;
+  font-family: "Lato", sans-serif;
+  display: flex;
+  align-items: center;
+  gap: 1rem;
+  margin-top: 30px;
+`;
+const Imagen = styled.img`
+  display: block;
+  width: 120px;
+`;
+const Texto = styled.p`
+  font-size: 18px;
+  span {
+    font-weight: 700;
+  }
+`;
+const Precio = styled.p`
+  font-size: 24px;
+  span {
+    font-weight: 700;
+  }
+`;
 
 const Resultado = ({ cotizacion }) => {
-  const { PRICE, HIGHDAY, LOWDAY, CHANGEPCT24HOUR, LASTUPDATE } = cotizacion;
+  const { PRICE, HIGHDAY, LOWDAY, CHANGEPCT24HOUR, IMAGEURL, LASTUPDATE } =
+    cotizacion;
   return (
-    <div>
-      <p>
-        El precio es de: <span>{PRICE}</span>
-      </p>
-      <p>
-        Precio más alto del día: <span>{HIGHDAY}</span>
-      </p>
-      <p>
-        Precio más bajo del día: <span>{LOWDAY}</span>
-      </p>
-      <p>
-        Variación ultimas 24 horas: <span>{CHANGEPCT24HOUR}</span>
-      </p>
-      <p>
-        Última actualización: <span>{LASTUPDATE}</span>
-      </p>
-    </div>
+    <Data>
+      <Imagen
+        src={`https://cryptocompare.com/${IMAGEURL}`}
+        alt="imagen cripto"
+      />
+      <div>
+        <Precio>
+          El precio es de: <span>{PRICE}</span>
+        </Precio>
+        <Texto>
+          Precio más alto del día: <span>{HIGHDAY}</span>
+        </Texto>
+        <Texto>
+          Precio más bajo del día: <span>{LOWDAY}</span>
+        </Texto>
+        <Texto>
+          Variación ultimas 24 horas: <span>{CHANGEPCT24HOUR}</span>
+        </Texto>
+        <Texto>
+          Última actualización: <span>{LASTUPDATE}</span>
+        </Texto>
+      </div>
+    </Data>
   );
 };
 
